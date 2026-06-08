83aed0913b4d1b5104d81787b04da605e67f73c8
LingyueWang <curryandre332@gmail.com>
2026-06-08 16:55:08 +0100

    modified assignment2 v1

---
+++ assignment2/plot.py
@@ -0,0 +1,46 @@
+# !/usr/bin/python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# parameters to modify 
+filename="time_c.txt"
+label='C'
+xlabel = 'time diff'
+ylabel = 'CDF'
+title='CDF rdtime C-1CPU plot'
+fig_name='rdtime_CDF_C_1CPU.png'
+bins=100 #adjust the number of bins to your plot
+
+## load data from input file
+t = np.loadtxt(filename, delimiter=" ", dtype="float")
+
+## if your data is "X Y" (2 cols), use the following line
+#plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+
+## if your data is "X" (1 col), use the following line
+#plt.plot(t, label=label)  # Plot some data on the (implicit) axes.
+
+## comment the lines above and uncomment the line below to plot a simple CDF
+#plt.hist(t[:], bins, density=True, histtype='step', cumulative=True, label=label)
+
+## comment the lines above and uncomment the 4 lines below for a nicer CDF
+n = np.arange(1,len(t)+1) / float(len(t))
+ts = np.sort(t)
+fig, ax = plt.subplots()
+ax.step(ts,n)
+minimum = np.percentile(ts, 0)
+median = np.percentile(ts, 50)
+a90th_percentile = np.percentile(ts, 90)
+a99th_percentile = np.percentile(ts, 99)
+maximum = np.percentile(ts, 100)
+average = np.mean(ts)
+print("minimum = ", minimum, "median = ", median, "90th = ", a90th_percentile, "99th = ", a99th_percentile, "maximum =", maximum, "average = ", average)
+
+
+plt.xlabel(xlabel)
+plt.ylabel(ylabel)
+plt.title(title)
+plt.legend()
+plt.savefig(fig_name)
+plt.show()
