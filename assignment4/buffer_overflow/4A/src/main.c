8cda681a8eea57afc89c4f3a5e86fcc119a3c855
LingyueWang <curryandre332@gmail.com>
2026-06-10 12:37:48 +0100

    assignment4 update

--- assignment4/buffer_overflow/4A/src/main.c
+++ assignment4/buffer_overflow/4A/src/main.c
@@ -106,9 +106,13 @@ int main(int argc, char* argv[])
     char evil_str[128];
     for (int j = 0; j < 128; j++) evil_str[j] = 0x41;  // Fill entire buffer
     
-    append_address(evil_str, address_start_byte, function_addr);
-    // hexdump_arr(evil_str);   // Uncomment for debug
-
+    //append_address(evil_str, address_start_byte, function_addr);
+    append_address(evil_str, 0, 0xdeadbeef);
+    hexdump_arr(evil_str);   // Uncomment for debug
+    printf("buf[0]: %p\n", (void *)&evil_str[0]);
+    printf("buf[1]: %p\n", (void *)&evil_str[1]);
+    printf("buf[2]: %p\n", (void *)&evil_str[2]);
+    printf("buf[3]: %p\n", (void *)&evil_str[3]);
     // Call the victim with normal input
     victim("0123456789ABCDE"); // Normal input of expected length == 16. No issues here
 
