e93de030b3e1f64a8d397a7123bc44383dbda4da
LingyueWang <curryandre332@gmail.com>
2026-06-11 10:05:06 +0100

    assignment 4 completed

--- assignment4/side_channel/login/views.py
+++ assignment4/side_channel/login/views.py
@@ -35,19 +35,20 @@ def safe_check(username: str, password: str) -> bool:
     if username != SECRET_USERNAME:
         return False
 
-    result = True
+    result = len(password) == len(SECRET_PASSWORD)
 
-    for i, ch in enumerate(password):
+    for i in range(len(SECRET_PASSWORD)):
         # i == index of the current character
         # ch == current character
-        
+        guess_char = password[i] if i < len(password) else "\0"
+
         # code below is vulnerable. Fix it!
         # then replace _vulnerable_check with safe_check in login_view() below
-        if i >= len(SECRET_PASSWORD) or ch != SECRET_PASSWORD[i]:
-            return False
-        time.sleep(DELAY_PER_CHAR)
+        if guess_char != SECRET_PASSWORD[i]:
+            result =  False
+       
 
-    return len(password) == len(SECRET_PASSWORD)
+    return result
 
 
 
@@ -61,7 +62,7 @@ def login_view(request):
         password = request.POST.get("password", "")
 
         t0 = time.perf_counter()
-        success = _vulnerable_check(username, password) # replace with safe_check as neede!
+        success = safe_check(username, password) # replace with safe_check as neede!
         elapsed_ms = (time.perf_counter() - t0) * 1000
 
         if success:
